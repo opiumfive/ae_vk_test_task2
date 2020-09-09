2aa18c685b9e96fad39f35c973b960016a13f069
opiumfive <opiumfive@gmail.com>
2020-09-09 15:16:40 +0000

    Deploying to gh-pages from  @ 989483d29c48d68f642934c6476ea874c59ce791 🚀

---
+++ precache-manifest.6ea80dc3e2b524f983fd206b49545b26.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0682ad9ca069d37f7f974044fff39370",
+    "url": "/ae_vk_test_task2/index.html"
+  },
+  {
+    "revision": "6c1d6f255556aefd6d48",
+    "url": "/ae_vk_test_task2/static/css/main.a1107254.chunk.css"
+  },
+  {
+    "revision": "ece8c704ab28d8ad7324",
+    "url": "/ae_vk_test_task2/static/js/2.9aaa8ee6.chunk.js"
+  },
+  {
+    "revision": "41b66b6fd6eecba581340794bb1831be",
+    "url": "/ae_vk_test_task2/static/js/2.9aaa8ee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c1d6f255556aefd6d48",
+    "url": "/ae_vk_test_task2/static/js/main.a5a411ce.chunk.js"
+  },
+  {
+    "revision": "a6b1d03272079bee4af5",
+    "url": "/ae_vk_test_task2/static/js/runtime-main.ee376547.js"
+  },
+  {
+    "revision": "dfc54362fc2b0f7b99a4038b87dd9e64",
+    "url": "/ae_vk_test_task2/static/media/1.dfc54362.png"
+  },
+  {
+    "revision": "2048975925bd8553d8aaa5e1d9d1ffb1",
+    "url": "/ae_vk_test_task2/static/media/2.20489759.png"
+  },
+  {
+    "revision": "dbf82c38432d45de3c1c99f13c8b5fee",
+    "url": "/ae_vk_test_task2/static/media/3.dbf82c38.png"
+  },
+  {
+    "revision": "e671d53961f44185ee61256a0e6f13a5",
+    "url": "/ae_vk_test_task2/static/media/4.e671d539.png"
+  },
+  {
+    "revision": "35bcd06768ffafae4a5721b8f0c4fd8f",
+    "url": "/ae_vk_test_task2/static/media/burger.35bcd067.png"
+  },
+  {
+    "revision": "06277842ccc3f7c2a37085189dacdcbc",
+    "url": "/ae_vk_test_task2/static/media/check-symbol.06277842.svg"
+  },
+  {
+    "revision": "751f84c25a8d1ff159f499cbb9aa5bb7",
+    "url": "/ae_vk_test_task2/static/media/edit.751f84c2.svg"
+  },
+  {
+    "revision": "4fd2ba47b4f59d0a4382a8d81aae85d6",
+    "url": "/ae_vk_test_task2/static/media/kfc.4fd2ba47.png"
+  },
+  {
+    "revision": "60464810e8cfcde7a20fc19284c38a0e",
+    "url": "/ae_vk_test_task2/static/media/mcdac.60464810.png"
+  },
+  {
+    "revision": "1198ed41690cc0974aa8baa0e95ebd1b",
+    "url": "/ae_vk_test_task2/static/media/refresh-button.1198ed41.svg"
+  },
+  {
+    "revision": "4eb05fdd9f3d81f2f81cac5847e25cc9",
+    "url": "/ae_vk_test_task2/static/media/sub.4eb05fdd.png"
+  }
+]);
