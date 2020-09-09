479999fa96576a2fd61450c55922e2076a138d05
opiumfive <allswell@bk.ru>
2020-09-09 20:07:47 +0300

    fix

--- src/panels/Basket.js
+++ src/panels/Basket.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useMemo } from "react";
 import { withRouter, Link } from "react-router-dom";
 import accounting from "accounting";
 
